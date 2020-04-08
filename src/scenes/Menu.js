913db2981ce1f7a43f51035965f19811c10499ad
jeremywei1 <jerjwei@gmail.com>
2020-04-08 15:47:43 -0700

    Scenes & assets implemented

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,12 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+
+    create() {
+        console.log(this);
+        // display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        //launch the next scene
+        this.scene.start('playScene');
+    }
 }
---
+++ src/scenes/Play.js
@@ -0,0 +1,34 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload() {
+        // load images / title sprite
+        // preload.image('fileName', 'location')
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+
+        
+        // white rectangle borders
+        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        // green UI background
+        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
+    }
+
+    update() {
+        // scroll starfields
+        this.starfield.tilePositionX -= 2.5;
+        // how to do diagnal movement?
+        // this.starfield.tilePositionY -= 2.5;
+    }
+}
