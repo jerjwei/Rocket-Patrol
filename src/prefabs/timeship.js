0bba7ee0b4dc5006724849d44ec341108e11f72b
JIA WEI <JWEI22@ucsc.edu>
2020-04-15 20:14:06 -0700

    spaceship update

--- src/prefabs/timeship.js
+++ src/prefabs/timeship.js
@@ -1,4 +1,4 @@
-//SuperSS prefab
+//timeship prefab
 class SuperSS extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
@@ -9,7 +9,7 @@ class SuperSS extends Phaser.GameObjects.Sprite {
 
     update() {
         // move spaceship left
-        this.x -= game.settings.supershipSpeed;
+        this.x -= 8;//game.settings.supershipSpeed;
         // wraparound screen bounds
         if(this.x <= 0 - this.width){
             this.x = game.config.width;
