d4a1afafff2c56b69b6178c25d538b2050f5e659
JIA WEI <JWEI22@ucsc.edu>
2020-04-18 03:33:31 -0700

    final version

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 // game configuration object
+// Add your own (copyright-free) background music to the Play scene (10)
+// Create a new scrolling tile sprite for the background (10)
+// Allow the player to control the Rocket after it's fired (10)
+// Create a new animated sprite for the Spaceship enemies (15)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (25)
+// Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (25) 
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits (25)
+// should be 120 in total
 let config = {
     type: Phaser.CANVAS,
     width: 640,
